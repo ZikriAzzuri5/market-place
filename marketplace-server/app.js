f84df47d46b00663ea3fdcd505f5bafc9444ee20
zikri azzuri <zikriazzuri5@gmail.com>
2024-10-01 15:27:09 +0700

    updated 2

--- marketplace-server/app.js
+++ marketplace-server/app.js
@@ -15,7 +15,13 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.use(cors());
+const corsOptions = {
+  origin: "https://market-place-orpin.vercel.app", // Frontend Anda
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- marketplace-server/database/index.js
+++ marketplace-server/database/index.js
@@ -7,10 +7,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 // );
 
 mongoose
-  .connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(MONGODB_URI, {})
   .then(() => console.log("MongoDB Atlas connected successfully"))
   .catch((err) => console.error("MongoDB connection error: ", err));
 
