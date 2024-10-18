fc5907facc77cd4130826926635ea4b414ac91d9
zikri azzuri <zikriazzuri5@gmail.com>
2024-10-18 10:07:30 +0700

    refactor gitignore

---
+++ marketplace-server/utils/errorHandler.js
@@ -0,0 +1,8 @@
+const handleValidationError = (err, res) => {
+  return res.status(400).json({
+    success: false,
+    message: err.details.map((e) => e.message).join(", "),
+  });
+};
+
+module.exports = { handleValidationError };
